344b907b6ad6c2aaa08ccee51ab54531695fab58
fikririsyad <fikri.risyad10@gmail.com>
2024-05-01 16:28:14 +0700

    [GREEN] Add TransactionItem model

---
+++ src/main/java/id/ac/ui/cs/advprog/hoomgroomproduct/model/TransactionItem.java
@@ -0,0 +1,20 @@
+package id.ac.ui.cs.advprog.hoomgroomproduct.model;
+
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Getter
+public class TransactionItem {
+    private final UUID id;
+    private final String name;
+    private final double price;
+    private final int quantity;
+
+    public TransactionItem(UUID id, String name, double price, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
