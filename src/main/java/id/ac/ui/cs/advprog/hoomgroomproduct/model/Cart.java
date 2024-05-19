350b908df94a372a11d5ca831197b72a260d2f97
fikririsyad <fikri.risyad10@gmail.com>
2024-05-19 14:49:26 +0700

    [GREEN] Implement wallet in cart

--- src/main/java/id/ac/ui/cs/advprog/hoomgroomproduct/model/Cart.java
+++ src/main/java/id/ac/ui/cs/advprog/hoomgroomproduct/model/Cart.java
@@ -2,28 +2,29 @@ package id.ac.ui.cs.advprog.hoomgroomproduct.model;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.*;
 
 @Getter
-@Setter
 @Entity
+@NoArgsConstructor
 public class Cart {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
     private Long userId;
 
+    @Setter
+    private double wallet;
+
     @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
     private List<CartItem> items;
-
-    public Cart() {
-
-    }
 
     public Cart(Long userId) {
         this.userId = userId;
         this.items = new ArrayList<>();
+        this.wallet = 0;
     }
 }
