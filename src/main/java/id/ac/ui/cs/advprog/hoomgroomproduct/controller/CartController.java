326f52a8d3dbfb683c570e2ea6673d02df8231b9
fikririsyad <fikri.risyad10@gmail.com>
2024-05-23 17:42:28 +0700

    [GREEN] Implement getCart and topUp

--- src/main/java/id/ac/ui/cs/advprog/hoomgroomproduct/controller/CartController.java
+++ src/main/java/id/ac/ui/cs/advprog/hoomgroomproduct/controller/CartController.java
@@ -1,6 +1,7 @@
 package id.ac.ui.cs.advprog.hoomgroomproduct.controller;
 
 import id.ac.ui.cs.advprog.hoomgroomproduct.dto.CartDto;
+import id.ac.ui.cs.advprog.hoomgroomproduct.dto.TopUpDto;
 import id.ac.ui.cs.advprog.hoomgroomproduct.model.Cart;
 import id.ac.ui.cs.advprog.hoomgroomproduct.service.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,33 +16,33 @@ public class CartController {
     @Autowired
     CartService cartService;
 
+    @GetMapping("/{userId}")
+    public ResponseEntity<Cart> getCart(@PathVariable Long userId) {
+        Cart cart = cartService.getCart(userId);
+        return ResponseEntity.ok(cart);
+    }
+
     @PostMapping("/add-items")
     public ResponseEntity<String> addItemToCart(@RequestBody CartDto request) {
-        try {
-            cartService.addItemToCart(request);
-            return ResponseEntity.ok("Items added to cart succesfully");
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-        }
+        cartService.addItemToCart(request);
+        return ResponseEntity.ok("Items added to cart succesfully");
     }
 
     @DeleteMapping("/delete-items")
     public ResponseEntity<String> deleteItemFromCart(@RequestBody CartDto request) {
-        try {
-            cartService.deleteItemFromCart(request);
-            return ResponseEntity.ok("Items deleted from cart succesfully");
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-        }
+        cartService.deleteItemFromCart(request);
+        return ResponseEntity.ok("Items deleted from cart succesfully");
     }
 
     @PostMapping("/clear-cart/{userId}")
     public ResponseEntity<String> clearCart(@PathVariable Long userId) {
-        try {
-            cartService.clearCart(userId);
-            return ResponseEntity.ok("Cart cleared successfully");
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-        }
+        cartService.clearCart(userId);
+        return ResponseEntity.ok("Cart cleared successfully");
+    }
+
+    @PostMapping("/topup")
+    public ResponseEntity<String> topUpWallet(@RequestBody TopUpDto request) {
+        cartService.topUpWallet(request);
+        return ResponseEntity.ok("Wallet updated successfully");
     }
 }
