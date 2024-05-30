1fd0de569c3f327e525ee964b331a07d75b988ba
Austin <austinsusanto@gmail.com>
2024-05-30 15:07:53 +0700

    [REFACTOR] Fixed code smells & optimized imports.

--- src/test/java/id/ac/ui/cs/advprog/hoomgroomproduct/controller/TransactionControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/hoomgroomproduct/controller/TransactionControllerTest.java
@@ -16,13 +16,11 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-
-import javax.print.attribute.standard.Media;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doReturn;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
